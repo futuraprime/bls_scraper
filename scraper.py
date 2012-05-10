acd0fbcc31aa9a1ab1cc9c8103c0689dc2a4427c
Evan <futuraprime@gmail.com>
2012-05-10 08:23:02 -0400

    Added basic CSV parsing.

--- scraper.py
+++ scraper.py
@@ -4,8 +4,29 @@ from bs4 import BeautifulSoup as Soup
 from sys import argv
 import urllib, urllib2
 import StringIO, csv
+    
+class UnicodeCsvReader(object):
+    def __init__(self, f, encoding="utf-8", **kwargs):
+        self.csv_reader = csv.reader(f, **kwargs)
+        self.encoding = encoding
 
-series = argv[1] if len(argv) > 1 else 'LNS12000000'
+    def __iter__(self):
+        return self
+
+    def next(self):
+        # read and split the csv row into fields
+        row = self.csv_reader.next() 
+        # now decode
+        return [unicode(cell, self.encoding) for cell in row]
+
+    @property
+    def line_num(self):
+        return self.csv_reader.line_num
+
+class UnicodeDictReader(csv.DictReader):
+    def __init__(self, f, encoding="utf-8", fieldnames=None, **kwds):
+        csv.DictReader.__init__(self, f, fieldnames=fieldnames, **kwds)
+        self.reader = UnicodeCsvReader(f, encoding=encoding, **kwds)
 
 def getData(series):
     postData = {
@@ -41,13 +62,30 @@ def getData(series):
         if active is True:
             csvRawList.append(line)
     
-    return '\n'.join(csvRawList)
+    return '\n'.join(csvRawList).encode('UTF-8')
 
 # print csvRawList
-
-outfile = open(series + '.csv', 'w')
-outfile.write(getData(series).encode('UTF-8'))
-outfile.close()
+def writeSeries(series):
+    outfile = open(series + '.csv', 'w')
+    outfile.write(getData(series))
+    outfile.close()
 
 # actually we want string IO to get the csv parsing to work...
 # see http://stackoverflow.com/questions/3305926/python-csv-string-to-array
+
+def parseSeries(series):
+    scsvRaw = getData(series)
+    f = StringIO.StringIO(scsvRaw)
+    reader = UnicodeDictReader(f)
+    data = [ r for r in reader ]
+    return data
+
+
+if __name__ == "__main__":
+    # LNS12000000 is the base unemployment rate
+    series = argv[1] if len(argv) > 1 else 'LNS12000000'
+
+    if len(argv) > 2 and argv[2] == 'file':
+        writeSeries(series)
+    else:
+        print parseSeries(series)
